fa43d01d020a76d2399f289e48316b0fa73c0a72
Shanae Leslie <sleslie18@gmail.com>
2022-10-27 18:36:38 -0400

    implemented search filter functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import dummyData from "./MOCK_DATA.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." />
-      {dummyData.map((val) => {
-        return (
-          <div className="contact" key={val.id}>
-            <p>{val.first_name}</p>
-          </div>
-        );
-      })}
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearchTerm(e.target.value);
+        }}
+      />
+      {dummyData
+        .filter((val) => {
+          if (!searchTerm) {
+            return val;
+          } else if (
+            val.first_name.toLowerCase().substring(0, searchTerm.length) ===
+            searchTerm.toLowerCase()
+          )
+            return val;
+        })
+        .map((val) => {
+          return (
+            <div className="contact" key={val.id}>
+              <p>{val.first_name}</p>
+            </div>
+          );
+        })}
     </div>
   );
 }
