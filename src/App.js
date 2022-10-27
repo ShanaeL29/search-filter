20083f9f7412355c208f04c4ce1aaa0b0898aba0
Shanae Leslie <sleslie18@gmail.com>
2022-10-27 17:15:50 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import dummyData from "./MOCK_DATA.json";
+
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Search..." />
+      {dummyData.map((val) => {
+        return (
+          <div className="contact" key={val.id}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
